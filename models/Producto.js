3baaf2d9c68edadb25b93bfc023333c8388ef528
juanpixelesyt <juanpixelesyt@gmail.com>
2025-05-15 19:48:45 -0500

    Delete models directory

--- models/Producto.js
+++
@@ -1,8 +0,0 @@
-const mongoose=requiere('mongoose');
-
-const itemSchema=new mongoose.Schema({
-    nombre:{type:String,require:true},
-    descripcion:string,
-    creadoEn:{type:Date,default:Date.now}
-});
-module.exports=mongoose.model('item',itemSchema)
